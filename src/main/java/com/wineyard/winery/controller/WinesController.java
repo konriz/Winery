209c2f00324c3528068cefc008e5bf173a698a69
konriz <konriz@gmail.com>
2018-02-19 20:27:25 +0100

    colour and taste mappings

--- src/main/java/com/wineyard/winery/controller/WinesController.java
+++ src/main/java/com/wineyard/winery/controller/WinesController.java
@@ -1,15 +1,16 @@
 package com.wineyard.winery.controller;
 
+import com.wineyard.winery.entity.Colour;
+import com.wineyard.winery.entity.Taste;
 import com.wineyard.winery.entity.Wine;
 import com.wineyard.winery.repository.WineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("wines")
+@RequestMapping("/wines")
 public class WinesController {
 
     @Autowired
@@ -18,6 +19,34 @@ public class WinesController {
     @GetMapping
     public List<Wine> getWines() {
         return wineRepository.findAll();
+    }
+
+    @RequestMapping("/tastes/{taste}")
+    public List<Wine> getTastes(@PathVariable("taste") String taste)
+    {
+        try
+        {
+            return wineRepository.findByTasteID(Taste.valueOf(taste.toUpperCase()));
+        }
+        catch (IllegalArgumentException e)
+        {
+            return wineRepository.findByTasteID(Taste.NONE);
+        }
+
+    }
+
+    @RequestMapping("/colours/{colour}")
+    public List<Wine> getColours(@PathVariable("colour") String colour)
+    {
+        try
+        {
+            return wineRepository.findByColourID(Colour.valueOf(colour.toUpperCase()));
+        }
+        catch (IllegalArgumentException e)
+        {
+            return wineRepository.findByColourID(Colour.NONE);
+        }
+
     }
 
 }
--- src/main/java/com/wineyard/winery/repository/WineRepository.java
+++ src/main/java/com/wineyard/winery/repository/WineRepository.java
@@ -1,9 +1,17 @@
 package com.wineyard.winery.repository;
 
+import com.wineyard.winery.entity.Colour;
+import com.wineyard.winery.entity.Taste;
 import com.wineyard.winery.entity.Wine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WineRepository extends JpaRepository<Wine, Integer>{
+
+
+    List<Wine> findByTasteID(Taste taste);
+    List<Wine> findByColourID(Colour colour);
 
 
 }
